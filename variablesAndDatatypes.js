1cbb896a2319a4d175be3ac8172cd26a32bf8648
Arman477Sharma <as6130457@gmail.com>
2026-08-14 22:09:19 +0530

    Varibles and data types

---
+++ variablesAndDatatypes.js
@@ -0,0 +1,16 @@
+console.log("Arman Sharma");
+age = 24;
+console.log(age+1);
+name="Arman Sharma";
+console.log(name);
+price=99.44;
+console.log(price);
+// rules
+// js is case sensitive. a & A have different meaning
+// only letter (_) and $ are allowed
+// reserved word cannot used in name of varibles 
+
+// Let const var
+// var can be re declared & updated. A global scope varibales
+// let variables cannot be re decalred but can be updated. A block scope variable
+// const variables cannot be re declared or updated . A block scope variables
