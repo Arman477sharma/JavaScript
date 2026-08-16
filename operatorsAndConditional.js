22b1f032a3f8c9d3ff6e99b103bc876afc6bfe0b
Arman477Sharma <as6130457@gmail.com>
2026-08-16 22:52:12 +0530

    operator and conditional statement

---
+++ operatorsAndConditional.js
@@ -0,0 +1,19 @@
+// comments 
+// part of code which is not executed
+/* multi line code */
+
+// operators: used to performs some operation(+,-,*,/ ,%,++,--)
+let a = 5;
+let b = 6;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a+b=",a+b);
+console.log("a%b=",a%b); // gives the reminder 
+console.log("a**b=",a**b); // power 
+a++;
+console.log("a++=",a); // increment by 1
+
+b--;
+console.log(b);
+
+
